d7df842859cedf03a260e54de6a8f33a953f9a8c
Juan Carlos Campos <rapidclimate@gmail.com>
2020-02-14 15:30:26 -0600

    Ya esta listo

--- src/main/java/org/unitec/invierno/Estatus.java
+++ src/main/java/org/unitec/invierno/Estatus.java
@@ -12,4 +12,23 @@ package org.unitec.invierno;
 public class Estatus {
     String mensaje;
     boolean success;
+
+    public Estatus() {
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
